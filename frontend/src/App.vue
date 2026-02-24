3357b648a9d5152a191a5c0ef631de3f386d3c79
mirzaev.arsen <arsen.00z11@gmail.com>
2026-02-24 20:59:57 +0300

    fix: move .gitignore

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+interface User {
+  name: string
+  email: string
+}
+
+const commonUser: User = {
+  name: 'Hello World',
+  email: 'admin@gmail.com'
+}
+</script>
+
+<template>
+  <div>{{ commonUser.name }}</div>
+</template>
+
+<style scoped>
+</style>
